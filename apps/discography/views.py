fe0f6f4ae9dcdc98f44d200ba9c028f7ec62466a
Roshan Shrestha <roshanshrestha01@gmail.com>
2016-05-08 17:34:06 +0545

    album list in discography template

--- apps/discography/views.py
+++ apps/discography/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from models import Album
+
+
+def discography(request):
+    context = {'albums': Album.objects.all()}
+    return render(request, 'discography/discography.html', context)
 
 # Create your views here.
