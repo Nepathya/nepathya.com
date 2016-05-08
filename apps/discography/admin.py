669e796d36c4b519e6c170fbe833f71562d90dad
Roshan Shrestha <roshanshrestha01@gmail.com>
2016-05-08 17:16:01 +0545

    Discography app model complete

--- apps/discography/admin.py
+++ apps/discography/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Track, Discography, Album
+from .models import Track, Album, Label, Genre
 
 admin.site.register(Track)
 admin.site.register(Album)
-admin.site.register(Discography)
+admin.site.register(Label)
+admin.site.register(Genre)
+# admin.site.register(Discography)
 
 # Register your models here.
--- apps/discography/models.py
+++ apps/discography/models.py
@@ -3,26 +3,37 @@ from __future__ import unicode_literals
 from django.db import models
 
 
+class Label(models.Model):
+    name = models.CharField(max_length=250)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=250)
+
+
 class Album(models.Model):
     title = models.CharField(max_length=250)
     description = models.CharField(max_length=250, blank=True, null=True)
+    cover_image = models.ImageField(blank=True, null=True)
+    year = models.PositiveIntegerField(blank=True, null=True)
+    released_date = models.DateField(blank=True, null=True)
+    label = models.ForeignKey(Label)
+    genre = models.ForeignKey(Genre, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
 
 
 class Track(models.Model):
     header = models.CharField(max_length=30)
     title = models.CharField(max_length=30)
-    artist = models.CharField(max_length=30)
-    album = models.CharField(max_length=50, blank=True, null=True)
-    year = models.IntegerField(blank=True, null=True)
+    artist = models.CharField(max_length=30, blank=True, null=True)
+    year = models.PositiveIntegerField(blank=True, null=True)
     comment = models.CharField(max_length=30, blank=True, null=True)
-    zero_byte = models.BooleanField(default=False)
-    track = models.IntegerField()
-    genre = models.CharField(max_length=255)
-    album_art = models.ImageField(blank=True, null=True)
+    track = models.PositiveSmallIntegerField(blank=True, null=True)
+    genre = models.ForeignKey(Genre, blank=True, null=True)
+    cover_image = models.ImageField(blank=True, null=True)
     copyright = models.CharField(max_length=200, blank=True, null=True)
-
-
-class Discography(models.Model):
     album = models.ForeignKey(Album, blank=True, null=True)
-    album_version = models.CharField(max_length=50, blank=True, null=True)
-    track = models.ForeignKey(Track, blank=True, null=True)
+    length = models.PositiveIntegerField(blank=True, null=True)
+    released_date = models.DateField(blank=True, null=True)
+    label = models.ForeignKey(Label, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
