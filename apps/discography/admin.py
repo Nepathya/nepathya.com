620f20ae20bb347ca07dbe5d4e90a55dd1e8145e
Roshan Shrestha <roshanshrestha01@gmail.com>
2016-05-08 16:33:23 +0545

    Ablum and Discography models

--- apps/discography/admin.py
+++ apps/discography/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Track
+from .models import Track, Discography, Album
 
 admin.site.register(Track)
+admin.site.register(Album)
+admin.site.register(Discography)
+
 # Register your models here.
--- apps/discography/models.py
+++ apps/discography/models.py
@@ -2,14 +2,27 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
+class Album(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.CharField(max_length=250, blank=True, null=True)
+
+
 class Track(models.Model):
-    header = models.CharField(max_length=3)
+    header = models.CharField(max_length=30)
     title = models.CharField(max_length=30)
     artist = models.CharField(max_length=30)
-    # album = models.ForeignKey(Album, blank=True, null=True)
+    album = models.CharField(max_length=50, blank=True, null=True)
     year = models.IntegerField(blank=True, null=True)
-    comment = models.CharField(max_length=30)
+    comment = models.CharField(max_length=30, blank=True, null=True)
     zero_byte = models.BooleanField(default=False)
     track = models.IntegerField()
     genre = models.CharField(max_length=255)
-# Create your models here.
+    album_art = models.ImageField(blank=True, null=True)
+    copyright = models.CharField(max_length=200, blank=True, null=True)
+
+
+class Discography(models.Model):
+    album = models.ForeignKey(Album, blank=True, null=True)
+    album_version = models.CharField(max_length=50, blank=True, null=True)
+    track = models.ForeignKey(Track, blank=True, null=True)
